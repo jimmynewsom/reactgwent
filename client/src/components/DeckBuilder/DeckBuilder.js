5fedc17b9fc39df34843f0d0de999c9e4b881040
Jimmy Newsom <jimmy.newsom@icloud.com>
2023-10-24 12:07:28 -0700

    extracting getCardData to global scope

--- client/src/components/DeckBuilder/DeckBuilder.js
+++ client/src/components/DeckBuilder/DeckBuilder.js
@@ -2,6 +2,7 @@ import './DeckBuilder.css';
 import React, { useState, useEffect } from 'react';
 import { useAuthHeader } from 'react-auth-kit';
 
+import {getCardData} from '../GwentClient/GwentClient';
 import {LargeCardView, CardData} from '../Card/Card';
 
 
@@ -237,40 +238,7 @@ export default function DeckBuilder() {
   //(Also, I need to build the map inside the async function, so there's a little duplicate code here)
   //then load the users decks from the server (or the default decks if the user has no saved decks, handled by server-side logic)
   useEffect(() => {
-    let cardRows;
-    if(localStorage.hasOwnProperty("cardRows")){
-      cardRows = JSON.parse(localStorage.getItem("cardRows"));
-      let map = new Map();
-      cardRows.forEach((row, i) => {
-        if(i !== 0){
-          let card = new CardData(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]);
-          map.set(row[0], card);
-        }
-      });
-      setCardMap(map);
-    }
-    else {
-      const fetchCardData = async () => {
-        try {
-          let result = await fetch("http://localhost:5000/getCardData", {
-            headers: {"Authorization": authHeader().split(" ")[1]}
-          });
-          cardRows = await result.json();
-          localStorage.setItem("cardRows", JSON.stringify(cardRows));
-          let map = new Map();
-          cardRows.forEach((row, i) => {
-            if(i !== 0){
-              let card = new CardData(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]);
-              map.set(row[0], card);
-            }
-          });
-          setCardMap(map);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-      fetchCardData();
-    }
+    getCardData(setCardMap, authHeader);
 
     const fetchUserDecks = async () => {
       try {
--- client/src/components/GwentClient/GwentClient.js
+++ client/src/components/GwentClient/GwentClient.js
@@ -1,15 +1,62 @@
 import React, { useState, useEffect } from 'react';
+import { useAuthHeader } from 'react-auth-kit';
+
+import {LargeCardView, CardData} from '../Card/Card';
 
 
-class GwentBoard{
-  constructor(){
+// class GwentBoard{
+//   constructor(){
     
+//   }
+// }
+
+
+export function getCardData(setcardmap, authheader) {
+  let cardRows;
+  if(localStorage.hasOwnProperty("cardRows")){
+    cardRows = JSON.parse(localStorage.getItem("cardRows"));
+    let map = new Map();
+    cardRows.forEach((row, i) => {
+      if(i !== 0){
+        let card = new CardData(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]);
+        map.set(row[0], card);
+      }
+    });
+    setcardmap(map);
+  }
+  else {
+    const fetchCardData = async () => {
+      try {
+        let result = await fetch("http://localhost:5000/getCardData", {
+          headers: {"Authorization": authheader().split(" ")[1]}
+        });
+        cardRows = await result.json();
+        localStorage.setItem("cardRows", JSON.stringify(cardRows));
+        let map = new Map();
+        cardRows.forEach((row, i) => {
+          if(i !== 0){
+            let card = new CardData(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]);
+            map.set(row[0], card);
+          }
+        });
+        setcardmap(map);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchCardData();
   }
 }
 
 
 export default function GwentClient() {
-  //const 
+  const authHeader = useAuthHeader();
+  const [cardMap, setCardMap] = useState(new Map());
+
+
+  useEffect(() => {
+    getCardData(setCardMap, authHeader);
+  }, []);
 
   return(
     <h2>Gwent Client</h2>
