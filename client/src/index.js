7fa684e6fe9475c10f883911241f7622fbdc4981
Jimmy Newsom <jimmy.newsom@icloud.com>
2023-10-31 16:19:20 -0700

    adding material ui

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,11 @@ import { AuthProvider } from 'react-auth-kit';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 import App from './components/App/App'
 
