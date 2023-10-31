18c2e0802733c5c8d9a22e4605e0f5a97eda495a
Jimmy Newsom <jimmy.newsom@icloud.com>
2023-10-31 14:28:19 -0700

    adding logic for initial database setup

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -9,6 +9,47 @@ const client = new MongoClient(DB_URL);
 var conn = await client.connect();
 var db = await conn.db("reactgwent");
 
+await db.collection("decks").createIndex("owner");
+
+let defaultDeck = {
+  "owner": "default",
+  "faction": "Northern Realms",
+  "cards": {
+    "Biting Frost": 2,
+    "Impenetrable Fog": 2,
+    "Torrential Rain": 2,
+    "Clear Weather": 2,
+    "Dethmold": 1,
+    "Trebuchet": 2,
+    "Ballista": 2,
+    "Ves": 1,
+    "Keira Metz": 1,
+    "Sile de Tansarville": 1,
+    "Prince Stennis": 1,
+    "Dun Banner Medic": 1,
+    "Sabrina Glevissig": 1,
+    "Sheldon Skaggs": 1,
+    "Blue Stripes Commando": 2,
+    "Poor Fucking Infantry": 2,
+    "Redanian Foot Soldier": 1,
+    "Kaedweni Siege Expert": 3,
+    "Yarpen Zigrin": 1,
+    "Siegfried of Denesle": 1
+  },
+  "totalCardCount": 30,
+  "totalUnitStrength": 84,
+  "heroCount": 0,
+  "leaderName": "Foltest, King of Temeria",
+  "specialCount": 8,
+  "unitCount": 22
+}
+
+let test = await db.collection().findOne({"owner": "default"});
+
+if(!test){
+  db.collection("decks").insertOne(defaultDeck);
+}
+
 console.log("successfully connected to the database");
 
 export default db;
