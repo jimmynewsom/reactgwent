aed6f52b49ff41f387c321130177d062175ad4e1
Jimmy Newsom <jimmy.newsom@icloud.com>
2023-10-31 16:27:40 -0700

    removing material ui...

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,6 @@ import { AuthProvider } from 'react-auth-kit';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 
 import App from './components/App/App'
 
