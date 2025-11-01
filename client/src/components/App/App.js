48f9edf63e3e53a75ab3f10330ab14928e648e6c
jimmynewsom <jimmy.newsom@icloud.com>
2025-11-01 16:01:53 -0700

    updating footer
    added cold start warning to footer since I removed it from my resume for space

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -90,7 +90,7 @@ export default function App() {
         <Route path="/smallcardview" element={<SmallCardView cardData={geralt} currentStrength={10}/>} />
         <Route path="/largecardview" element={<LargeCardView cardData={geralt} handleClick={() => console.log("nothing")} available={3}/>} />
       </Routes>
-      <footer>This is an unofficial, fan made game for demonstration purposes only, and is in no way endorsed by CD PROJEKT RED. It can also only play a maximum of 5 games at a time, and individual users can only play a maximum of 10 games per month.</footer>
+      <footer>This is currently running on free hosting, which means the back end might need time for a cold start. If you press login or register and nothing happens please wait 1 minute and try again. Sorry for the inconvenience. This is also an unofficial, fan made game for demonstration purposes only, and is in no way endorsed by CD PROJEKT RED.</footer>
     </div>
   );
 }
